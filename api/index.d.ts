794a3fd40bd1473887c466c46b9c0e87f3b12cf7
Rodrigo Brabo <brabo.rodrigo@gmail.com>
2021-10-05 21:16:15 -0300

    bundle

--- api/index.d.ts
+++ api/index.d.ts
@@ -1,7 +1,8 @@
+import Gateway from './gateway';
 declare const Router: {
     module: (routes: any) => {
         new (): {};
     };
     component: (...componentArgs: any[]) => (Target: any, config: any, options: any, customs: any, broadcast: any) => (moduleConfig: any, children: any) => (parentRouter: any) => void;
 };
-export { Router };
+export { Router, Gateway };
--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Router = void 0;
+exports.Gateway = exports.Router = void 0;
 const router_module_1 = require("./router.module");
 const router_component_1 = require("./router.component");
+const gateway_1 = __importDefault(require("./gateway"));
+exports.Gateway = gateway_1.default;
 const Router = {
     module: router_module_1.RouterModule,
     component: router_component_1.RouterComponent
