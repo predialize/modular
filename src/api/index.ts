b2547d018bbe2930ac1684aec445b1bd8a9985ef
Rodrigo Brabo <brabo.rodrigo@gmail.com>
2021-10-05 21:12:54 -0300

    export gateway

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
 import { RouterModule } from './router.module';
 import { RouterComponent } from './router.component';
+import Gateway from './gateway';
 
 const Router = {
    module: RouterModule,
    component: RouterComponent
 } 
 
-export { Router }
+export { Router, Gateway }
